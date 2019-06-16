6d8fd23058261e2d135d700676a06acfa154cc10
aindriu80 <aindriu80@gmail.com>
2019-06-16 20:48:22 +0100

    Children Property

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component{
   return (
     <div className="App">
      <h1>React - The Complete Guide (incl Hooks, React Router, Redux)</h1>
-     <Person />
+     <Person name="MAx" age="28"/>
+     <Person name="Manu" age="29"> My hobbies: Racing </Person>
+     <Person name="Stephanie" age="25"/>
     </div>
   );
   // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work now?'));
